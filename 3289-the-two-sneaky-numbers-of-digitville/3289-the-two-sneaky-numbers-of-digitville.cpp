bac01122554728749e3fd8e4966ef7e9d87db56a
Ashish sharma <152724525+Ashishsharma0010@users.noreply.github.com>
2025-10-31 20:54:49 +0530

    Time: 3 ms (40.61%), Space: 26 MB (57.88%) - LeetHub

---
+++ 3289-the-two-sneaky-numbers-of-digitville/3289-the-two-sneaky-numbers-of-digitville.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> getSneakyNumbers(vector<int>& nums) {
+  int n = nums.size();
+        vector<int> freq(n, 0);
+             vector<int> result;
+              for (int num : nums) {
+                  freq[num]++;
+                    if (freq[num] == 2) {
+                        result.push_back(num);
+                     }
+                              }
+       return result;
+        
+    }
+};
