e818858b9dca72cbce80e847687c4d7200d5c825
Ashish sharma <152724525+Ashishsharma0010@users.noreply.github.com>
2026-02-12 23:18:15 +0530

    Time: 0 ms (100%), Space: 12.7 MB (27.52%) - LeetHub

--- 0162-find-peak-element/0162-find-peak-element.cpp
+++ 0162-find-peak-element/0162-find-peak-element.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-        // brute force because they said time complexity should be O(LOG N)AND  i am doing in O(N)
-        int n=nums.size();
-        int peak;
-        for(int i=0; i<n; i++){
-            if((i==0 || nums[i-1]<nums[i]) && (i==n-1 || nums[i+1]<nums[i]))
-            return i;
-            
+    int n=nums.size();
+    if (n==1){
+        return 0;
+    }
+    if(nums[0]>nums[1]) return 0;
+    if(nums[n-1]>nums[n-2]) return n-1;
+     
+    int low=1, high=n-2;
+    while(low<=high){
+        int mid=(low+high)/2;
+        if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]){
+            return mid;
         }
-        return -1;
-        
+        else if (nums[mid]>nums[mid-1]) low=mid+1;
+        else high=mid-1;
+    }
+    return -1;
+    // time complexity is O(LOG N);
+   
     }
 };
