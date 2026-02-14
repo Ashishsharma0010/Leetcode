5bb8a667b3e4c983f340aa449513cbb31e6f4522
Ashish sharma <152724525+Ashishsharma0010@users.noreply.github.com>
2026-02-14 15:22:10 +0530

    Time: 0 ms (100%), Space: 17.6 MB (53.86%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,19 +1,48 @@
 class Solution {
 public:
     vector<int> searchRange(vector<int>& nums, int target) {
-        // Now i am trying to do by using the brute force method  by time complexity O(N) AND SPACE O(1)
-        vector<int> s(2,-1);
-        int n=nums.size();
-        for(int i=0; i<n; i++){
-            if(nums[i]==target){
-                if(s[0]==-1){
-                    s[0]=i;
-
-                }
-                s[1]=i;  // s[i] update kar diya maine s[0] se s[1]; next time s[1]==-1 
+        
+        int n = nums.size();
+        int low = 0, high = n - 1;
+        int first = -1;
+        
+        // Find FIRST position
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            if (nums[mid] == target) {
+                first = mid;
+                high = mid - 1;   // move left
+            }
+            else if (nums[mid] > target) {
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
             }
         }
-        return s; // last m return krdiya jo value aai hai
-
+        
+        
+        // Reset for LAST position
+        low = 0;
+        high = n - 1;
+        int last = -1;
+        
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            
+            if (nums[mid] == target) {
+                last = mid;
+                low = mid + 1;   // move right
+            }
+            else if (nums[mid] > target) {
+                high = mid - 1;
+            }
+            else {
+                low = mid + 1;
+            }
+        }
+        
+        return {first, last};
     }
 };
