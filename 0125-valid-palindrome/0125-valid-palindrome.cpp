faed7f7513747ba02812c052ab2ac251ab1eafe1
Ashish sharma <152724525+Ashishsharma0010@users.noreply.github.com>
2026-02-21 11:02:58 +0530

    Time: 0 ms (100%), Space: 9.8 MB (93.55%) - LeetHub

---
+++ 0125-valid-palindrome/0125-valid-palindrome.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public: 
+    bool  alphanumric(char ch){
+         
+         if((ch>='0' && ch<='9') || (tolower(ch)>='a' && tolower(ch)<='z')){
+            return true;
+         }
+         return false;
+
+    }
+    bool isPalindrome(string s) {
+        int start=0;
+        int end=s.size()-1;
+        while(start<=end){
+            if(!alphanumric(s[start])){
+                start++;
+                continue;
+            }
+            if(!alphanumric(s[end])){
+                end--;
+                continue;
+            }
+            if(tolower(s[start])!=tolower(s[end])){            return false;
+            }
+            start++;
+            end--;
+
+        }
+        return true;
+        
+    }
+};
