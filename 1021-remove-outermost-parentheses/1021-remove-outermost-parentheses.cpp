bf2067286afb6ef91274da7a6dea46cad93eaad0
Ashish sharma <152724525+Ashishsharma0010@users.noreply.github.com>
2026-03-21 15:29:16 +0530

    Time: 0 ms (100%), Space: 9 MB (54.57%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.cpp
@@ -4,15 +4,17 @@ public:
        string ans="";
        int count=0;
        for(int i=0; i<s.length(); i++){
-        if(s[i]==')') count--;
-        if(count!=0)
-            ans.push_back(s[i]);
-         if(s[i]=='(')
-        count++;
-
+        if(s[i]==')'){
+            count++; 
+        }
+        if(count!=0){
+         ans.push_back(s[i]);
+        }
+        if(s[i]=='('){
+            count--;
+        }
        }
-
-           return ans;
+       return ans;
         
     }
 };
