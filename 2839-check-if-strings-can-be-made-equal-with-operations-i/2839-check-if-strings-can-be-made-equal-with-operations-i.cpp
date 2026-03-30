bc077e247f0c83e1be36f1140e1cab133a2fa59e
Ashish sharma <152724525+Ashishsharma0010@users.noreply.github.com>
2026-03-30 18:45:44 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.cpp
+++ 2839-check-if-strings-can-be-made-equal-with-operations-i/2839-check-if-strings-can-be-made-equal-with-operations-i.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     bool canBeEqual(string s1, string s2) {
-      string a="";
+     string a="";
      string b="";
      a=a+s1[0];
      a=a+s1[2];
@@ -9,20 +9,16 @@ public:
      b=b+s2[2];
      sort(a.begin(),a.end());
      sort(b.begin(),b.end());
-     string c = "";
-        string d = "";
-        
-        c += s1[1];
-        c += s1[3];
-        
-        d += s2[1];
-        d += s2[3];
-        
-        sort(c.begin(), c.end());
-        sort(d.begin(), d.end());
-        
-        return (a == b && c == d);
-    
+     string c="";
+     string d="";
+     c=c+s1[1];
+     c=c+s1[3];
+     d=d+s2[1];
+     d=d+s2[3];
+     sort(c.begin(),c.end());
+     sort(d.begin(),d.end());
+     return (a==b && c==d);
+     
 
         
     }
